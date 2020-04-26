b839adc5d494ebb218d745eaaa4f89165b6b3b00
EV <vzhenia@users.noreply.github.com>
2020-04-26 07:03:09 +0300

    MD added

--- helpers/countries.js
+++ helpers/countries.js
@@ -3,5 +3,10 @@ const countries = {
     name: "Ukraine",
     capital: "Kyiv",
     coordinates: "50.4021351,30.2518195"
+  },
+  "MD": {
+    name: "Moldova",
+    capital: "Chișinău",
+    coordinates: "47.0000174,28.7177486"
   }
 }
